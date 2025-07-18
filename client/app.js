635b16da39b73f98ed29de610925e448e030b699
kowa77 <estebangarriga@hotmail.com>
2025-07-18 20:51:22 -0300

    Added branch master, added .gitignore .env

---
+++ client/app.js
@@ -0,0 +1,70 @@
+const mp = new MercadoPago("APP_USR-31042028-7376-4288-94b4-edd6a94f77c1", {
+    locale: "es-UY"
+});
+
+document.getElementById("checkout-btn").addEventListener("click", async ()=> {
+    try {
+        const orderData ={
+            title: "PELOTITA",
+            quantity: 1,
+            price: 100
+        };
+
+        const response = await fetch("https://servidor-js-mp-production.up.railway.app/create_preference", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(orderData)
+        });
+
+        const preference = await response.json();
+        console.log(preference);
+        createCheckoutButton(preference.id);
+
+    } catch (error) {
+        alert("Error al crear la preferencia"); 
+        console.error(error);
+    }   
+
+});
+
+const createCheckoutButton = (preferenceId) => {
+const bricksBuilder = mp.bricks();
+
+const renderComponent = async ( ) =>{
+    if(window.checkoutButton) window.checkoutButton.unmount();
+    
+//    await bricksBuilder.bricks().create("wallet", "wallet_container", {
+      await bricksBuilder.create("wallet", "wallet_container", {
+        initialization: {
+            preferenceId: preferenceId
+        },
+        customization: {
+            visual: {
+                theme: "dark",
+                color: "#000000",
+                logo: "https://www.mercadopago.com.uy/fp/checkout/checkout.png"
+            }
+        },
+        callbacks: {
+            onReady: () => {
+                console.log("Bricks initialized");
+            },
+            onError: (error) => {
+                console.error("Error initializing bricks", error);
+            }
+        }
+    });
+}
+    renderComponent();
+    //bricksBuilder.bricks().renderAll();
+    console.log("Bricks rendered");
+};
+
+
+
+
+
+
+    
---
+++ server/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+
+//SDK de Mercado Pago
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+// Agregando credenciales
+const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN });
+
+const app = express();
+const PORT = process.env.PORT || 3000; // ¡Este es el cambio importante!
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/create_preference', async (req, res) => {
+    try {
+        const body = {
+            items:[
+                {
+                title: req.body.title,
+                quantity: Number(req.body.quantity),
+                unit_price: Number(req.body.price),
+                currency_id: "UYU",
+                },
+            ],
+            back_urls: {
+                success: "https://www.elpais.com.uy/",
+                failure: "https://www.elpais.com.uy/",
+                pending: "https://www.elpais.com.uy/"
+            },
+            auto_return: "approved",
+        };
+        const preference = new Preference(client);
+        const result = await preference.create({body});
+        res.json({
+            id: result.id,
+        });
+    } catch (error) {
+        console.error('Error creating preference:', error);
+        res.status(500).json({ error: 'Error al crear la preferencia ;(' });
+        return;
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
